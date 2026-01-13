ce2e30d16afa820b5bdb426412574aa4c33a8c6d
Farhan Arshad <farhan.arshad@arbisoft.com>
2026-01-13 11:02:02 +0500

    refactor: Remove custom back-end

--- offmarket/settings.py
+++ offmarket/settings.py
@@ -90,10 +90,6 @@ DATABASES = {
     }
 }
 
-AUTHENTICATION_BACKENDS = [
-    'accounts.backends.EmailBackend',
-]
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
