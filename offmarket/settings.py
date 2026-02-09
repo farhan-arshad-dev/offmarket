52789472b1aa5a18953ad954cc76fe3677149dff
Farhan Arshad <farhan.arshad@arbisoft.com>
2026-02-09 12:58:21 +0500

    feat: API Throttling

--- offmarket/settings.py
+++ offmarket/settings.py
@@ -129,7 +129,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/min',
+        'user': '3/min'
+    }
 }
 
 SIMPLE_JWT = {
